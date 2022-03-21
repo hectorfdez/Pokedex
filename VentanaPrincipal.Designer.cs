b2274f81da1cb083d8acecdcb96fc839bf230ca9
xp <xp@CETYSDAM00>
2022-03-21 18:13:42 +0100

    Agregar archivos de proyecto.

---
+++ VentanaPrincipal.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace Pokedex
+{
+    partial class VentanaPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.izquierda = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.derecha = new System.Windows.Forms.Button();
+            this.nombrePokemon = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // izquierda
+            // 
+            this.izquierda.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.izquierda.Location = new System.Drawing.Point(43, 354);
+            this.izquierda.Name = "izquierda";
+            this.izquierda.Size = new System.Drawing.Size(121, 74);
+            this.izquierda.TabIndex = 0;
+            this.izquierda.Text = "<";
+            this.izquierda.UseVisualStyleBackColor = true;
+            this.izquierda.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(43, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 296);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // derecha
+            // 
+            this.derecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.derecha.Location = new System.Drawing.Point(170, 354);
+            this.derecha.Name = "derecha";
+            this.derecha.Size = new System.Drawing.Size(121, 74);
+            this.derecha.TabIndex = 2;
+            this.derecha.Text = ">";
+            this.derecha.UseVisualStyleBackColor = true;
+            this.derecha.Click += new System.EventHandler(this.derecha_Click);
+            // 
+            // nombrePokemon
+            // 
+            this.nombrePokemon.Location = new System.Drawing.Point(298, 39);
+            this.nombrePokemon.Name = "nombrePokemon";
+            this.nombrePokemon.Size = new System.Drawing.Size(203, 50);
+            this.nombrePokemon.TabIndex = 3;
+            this.nombrePokemon.Text = "label1";
+            // 
+            // VentanaPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 490);
+            this.Controls.Add(this.nombrePokemon);
+            this.Controls.Add(this.derecha);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.izquierda);
+            this.Name = "VentanaPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button izquierda;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button derecha;
+        private System.Windows.Forms.Label nombrePokemon;
+    }
+}
+
---
+++ VentanaPrincipal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pokedex
+{
+    public partial class VentanaPrincipal : Form
+    {
+        Conexion miConexion = new Conexion();
+        DataTable misPokememons = new DataTable();
+        int idActual = 1;//Guardo el id del pokemon
+        public VentanaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private Image convierteBlobAImagen(byte[] img)
+        {
+            MemoryStream ms = new System.IO.MemoryStream(img);
+            return (Image.FromStream(ms));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            idActual--;
+            misPokememons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokememons.Rows[0]["nombre"].ToString();
+        }
+
+        private void derecha_Click(object sender, EventArgs e)
+        {
+            idActual++;
+            misPokememons = miConexion.getPokemonPorId(idActual);
+            nombrePokemon.Text = misPokememons.Rows[0]["nombre"].ToString();
+            pictureBox1.Image = convierteBlobAImagen((byte[])misPokememons.Rows[0]["imagen"]);
+        }
+
+    }
+}
